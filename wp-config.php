97908513d0f5a91b4158e87ae79628a616634d59
ChristianNMdima <nc.mdima@gmail.com>
2021-08-04 23:43:20 +0200

    Db config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'vmprogeny' );
+define( 'DB_NAME', 'vmprogenydb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'vmprogenyuser' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Md1m@1990' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
